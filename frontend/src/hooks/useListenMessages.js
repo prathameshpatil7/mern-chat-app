90c88135b44266a4f2fa0b01f249b0af3d20fd7b
prathameshpatil7 <75564009+prathameshpatil7@users.noreply.github.com>
2024-06-28 23:36:33 +0530

    Update useListenMessages.js

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -6,18 +6,23 @@ import useConversation from "../zustand/useConversation";
 import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
-	const { socket } = useSocketContext();
-	const { messages, setMessages } = useConversation();
+  const { socket } = useSocketContext();
+  const { selectedConversation, messages, setMessages } = useConversation();
+  if (!selectedConversation._id) {
+    return;
+  }
+  useEffect(() => {
+    socket?.on("newMessage", (newMessage) => {
+      console.log(newMessage, selectedConversation);
+      if (newMessage.senderId === selectedConversation._id) {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      }
+    });
 
-	useEffect(() => {
-		socket?.on("newMessage", (newMessage) => {
-			newMessage.shouldShake = true;
-			const sound = new Audio(notificationSound);
-			sound.play();
-			setMessages([...messages, newMessage]);
-		});
-
-		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages]);
+    return () => socket?.off("newMessage");
+  }, [socket, setMessages, messages, selectedConversation._id]);
 };
 export default useListenMessages;
